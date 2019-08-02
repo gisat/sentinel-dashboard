0d9593ebe9ffe4e121e0d63ffc5d9b73f67bdfb6
Vojtěch Dubrovský <v.dubr@hotmail.com>
2019-08-02 13:51:01 +0200

    Add satellites icons

--- src/components/Icon/components/Location.js
+++ src/components/Icon/components/Location.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default props => (
     <g transform="matrix(.15695 0 0 .15695 -42.935 -16.345)">
-        <path d="m308.53 197c0-37.017 29.983-67 67-67 37.017 0 67 29.983 67 67s-29.983 67-67 67c-37.017 0-67-29.983-67-67z" fill="#ffffff" marker-end="" marker-mid="" marker-start="" stroke="#222222" stroke-dashoffset="" stroke-linejoin="round" stroke-width="7"/>
-        <path d="m273.56 198.05h69.668" fill="#ffffff" marker-end="" marker-mid="" marker-start="" stroke="#222222" stroke-dashoffset="" stroke-linejoin="round" stroke-width="7"/>
-        <path transform="rotate(-89.72 375.88 139)" d="m341.05 139h69.668" fill="#ffffff" marker-end="" marker-mid="" marker-start="" stroke="#222222" stroke-dashoffset="" stroke-linejoin="round" stroke-width="7"/>
-        <path transform="rotate(-89.72 375.5 264.68)" d="m340.67 264.68h69.668" fill="#ffffff" marker-end="" marker-mid="" marker-start="" stroke="#222222" stroke-dashoffset="" stroke-linejoin="round" stroke-width="7"/>
-        <path d="m407.78 198.14h69.668" fill="#ffffff" marker-end="" marker-mid="" marker-start="" stroke="#222222" stroke-dashoffset="" stroke-linejoin="round" stroke-width="7"/>
+        <path d="m308.53 197c0-37.017 29.983-67 67-67 37.017 0 67 29.983 67 67s-29.983 67-67 67c-37.017 0-67-29.983-67-67z" fill="#ffffff" markerEnd="" markerMid="" markerStart="" stroke="#222222" strokeDashoffset="" strokeLinejoin="round" strokeWidth="7"/>
+        <path d="m273.56 198.05h69.668" fill="#ffffff" markerEnd="" markerMid="" markerStart="" stroke="#222222" strokeDashoffset="" strokeLinejoin="round" strokeWidth="7"/>
+        <path transform="rotate(-89.72 375.88 139)" d="m341.05 139h69.668" fill="#ffffff" markerEnd="" markerMid="" markerStart="" stroke="#222222" strokeDashoffset="" strokeLinejoin="round" strokeWidth="7"/>
+        <path transform="rotate(-89.72 375.5 264.68)" d="m340.67 264.68h69.668" fill="#ffffff" markerEnd="" markerMid="" markerStart="" stroke="#222222" strokeDashoffset="" strokeLinejoin="round" strokeWidth="7"/>
+        <path d="m407.78 198.14h69.668" fill="#ffffff" markerEnd="" markerMid="" markerStart="" stroke="#222222" strokeDashoffset="" strokeLinejoin="round" strokeWidth="7"/>
     </g>
 );
