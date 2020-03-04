9c7c203362850722efb02d2e60116bca5ff83ac2
Vojtěch Dubrovský <v.dubr@hotmail.com>
2020-03-04 14:30:20 +0100

    New build version

---
+++ build/precache-manifest.1d934c9ec6e4d92af9868d56d31031d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2309c81957609bbe5bdb68f8fb77bbe0",
+    "url": "./b9232d3625614bd14794.worker.js"
+  },
+  {
+    "revision": "345db2989eafa622992269745ca59ff0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fdf593beaffb77903cf5",
+    "url": "./static/css/main.45a53e63.chunk.css"
+  },
+  {
+    "revision": "e3279a931a04a93f07f0",
+    "url": "./static/js/2.7225455e.chunk.js"
+  },
+  {
+    "revision": "6d610836f50f8b09eb5cffe6fc07ed2c",
+    "url": "./static/js/2.7225455e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf593beaffb77903cf5",
+    "url": "./static/js/main.4dded018.chunk.js"
+  },
+  {
+    "revision": "6e2910f5f6a9370be34c",
+    "url": "./static/js/runtime-main.5a6df461.js"
+  }
+]);
