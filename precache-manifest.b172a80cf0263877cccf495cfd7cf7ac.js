8a763480c82a31bdbf0fde7d04cc471ece53eca8
Vojtěch Dubrovský <v.dubr@hotmail.com>
2020-07-24 09:14:54 +0200

    Deploy updates

---
+++ precache-manifest.b172a80cf0263877cccf495cfd7cf7ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2309c81957609bbe5bdb68f8fb77bbe0",
+    "url": "./b9232d3625614bd14794.worker.js"
+  },
+  {
+    "revision": "935fdbd93099bdba4da64a75357f028b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76a5bcb9911322ee6634",
+    "url": "./static/css/main.505cabcf.chunk.css"
+  },
+  {
+    "revision": "bcb5d6c943fc669e8b19",
+    "url": "./static/js/2.92c75889.chunk.js"
+  },
+  {
+    "revision": "f3f03d0ca25f2c96cbca4ee14de34d75",
+    "url": "./static/js/2.92c75889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a5bcb9911322ee6634",
+    "url": "./static/js/main.09474241.chunk.js"
+  },
+  {
+    "revision": "6e2910f5f6a9370be34c",
+    "url": "./static/js/runtime-main.5a6df461.js"
+  }
+]);
